0c22b0c242e8efa4b46e985dab757a5e330d4832
Temulen GPG <temulen@Temulens-MacBook-Pro.local>
2024-03-10 23:19:25 +0800

    home

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -28,29 +28,60 @@ export const AddCategory = () => {
                   </div>
                   <ul
                     tabIndex={0}
-                    className="dropdown-content z-[1] menu shadow bg-base-100 rounded-box w-52 flex flex-col"
+                    className="dropdown-content z-[1] menu shadow bg-base-100 rounded-box w-[312px] h-[336px] flex flex-col"
                   >
-                    <div className="grid grid-cols-6 w-[84px]">
-                      <img src="./addCategoryIcons/House.svg" alt="" />
-                      <img src="./addCategoryIcons/HouseLine.svg" alt="" />
+                    <div className="grid grid-cols-6 w-[312px] h-[336px]">
+                      <img src="./addCategoryIcons/1-House.svg" alt="" />
+                      <img src="./addCategoryIcons/2-HouseLine.svg" alt="" />
                       <img
-                        src="./addCategoryIcons/IdentificationBadge.svg"
+                        src="./addCategoryIcons/3-IdentificationBadge.svg"
                         alt=""
                       />
                       <img
-                        src="./addCategoryIcons/IdentificationCard.svg"
+                        src="./addCategoryIcons/4-IdentificationCard.svg"
                         alt=""
                       />
-                      <img src="./addCategoryIcons/Ladder.svg" alt="" />
+                      <img src="./addCategoryIcons/5-Ladder.svg" alt="" />
                       <img
-                        src="./addCategoryIcons/IntersectSquare.svg"
+                        src="./addCategoryIcons/6-IntersectSquare.svg"
                         alt=""
                       />
-                      <img src="./addCategoryIcons/ImageSquare.svg" alt="" />
+                      <img src="./addCategoryIcons/7-ImageSquare.svg" alt="" />
                       <img
-                        src="./addCategoryIcons/MagnifyingGlassPlus.svg"
+                        src="./addCategoryIcons/8-MagnifyingGlassPlus.svg"
                         alt=""
                       />
+                      <img src="./addCategoryIcons/9-Microphone.svg" alt="" />
+                      <img src="./addCategoryIcons/10-MSExcel.svg" alt="" />
+                      <img src="./addCategoryIcons/11-Notepad.svg" alt="" />
+                      <img src="./addCategoryIcons/12-ListPlus.svg" alt="" />
+                      <img src="./addCategoryIcons/13-Leaf.svg" alt="" />
+                      <img src="./addCategoryIcons/14-Five.svg" alt="" />
+                      <img src="./addCategoryIcons/15-SevenCircle.svg" alt="" />
+                      <img src="./addCategoryIcons/16-RoadHorizon.svg" alt="" />
+                      <img
+                        src="./addCategoryIcons/17-HourGlassMid.svg"
+                        alt=""
+                      />
+                      <img
+                        src="./addCategoryIcons/18-AnchorSimple.svg"
+                        alt=""
+                      />
+                      <img src="./addCategoryIcons/19-Bezier.svg" alt="" />
+                      <img src="./addCategoryIcons/20-Exclude.svg" alt="" />
+                      <img src="./addCategoryIcons/21-Vignette.svg" alt="" />
+                      <img src="./addCategoryIcons/22-Baseball.svg" alt="" />
+                      <img
+                        src="./addCategoryIcons/23-QuestionCircle.svg"
+                        alt=""
+                      />
+                      <img src="./addCategoryIcons/24-Exam.svg" alt="" />
+                      <img src="./addCategoryIcons/25-Watch.svg" alt="" />
+                      <img src="./addCategoryIcons/26-Globe.svg" alt="" />
+                      <img src="./addCategoryIcons/27-OrangeSlice.svg" alt="" />
+                      <img src="./addCategoryIcons/28-Peace.svg" alt="" />
+                      <img src="./addCategoryIcons/29-ToiletPaper.svg" alt="" />
+                      <img src="./addCategoryIcons/30-Pencil.svg" alt="" />
                     </div>
                   </ul>
                 </div>
